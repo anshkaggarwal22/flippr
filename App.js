1f431963f4cf5354f2006a9eb67de0f178fe3f88
Alex <alexwang409@gmail.com>
2024-09-21 13:39:49 -0400

    interestsQuesstionaires

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import * as Font from 'expo-font';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import LandingPage from './Screens/LandingPage';
-import Signup from './Screens/SignUp';
-import Login from './Screens/Login';
-import InterestsQuestionnaire from './Screens/InterestsQuestionnaire';
-import Home from './Screens/Home';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LandingPage from "./Screens/LandingPage";
+import Signup from "./Screens/SignUp";
+import Login from "./Screens/Login";
+import InterestsQuestionnaire from "./Screens/InterestsQuestionnaire";
+import Home from "./Screens/Home";
 
 const Stack = createStackNavigator();
 
@@ -18,8 +18,8 @@ export default function App() {
   useEffect(() => {
     async function loadFonts() {
       await Font.loadAsync({
-        'ProximaNova-Bold': require('./assets/fonts/proximanova_bold.otf'),
-        'ProximaNova-Regular': require('./assets/fonts/proximanova_regular.ttf'),
+        "ProximaNova-Bold": require("./assets/fonts/proximanova_bold.otf"),
+        "ProximaNova-Regular": require("./assets/fonts/proximanova_regular.ttf"),
       });
       setFontsLoaded(true);
     }
@@ -35,15 +35,19 @@ export default function App() {
     <NavigationContainer>
       <StatusBar style="dark" />
       <Stack.Navigator
-        initialRouteName="InterestsQuestionnaire"
+        initialRouteName="LandingPage"
         screenOptions={{
-          headerShown: false, 
+          headerShown: false,
         }}
       >
         <Stack.Screen name="LandingPage" component={LandingPage} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="InterestsQuestionnaire" component={InterestsQuestionnaire} />
+        <Stack.Screen
+          name="InterestsQuestionnaire"
+          component={InterestsQuestionnaire}
+          initialParams={{ token: "dev-test-token-123" }}
+        />
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
@@ -53,8 +57,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E7EAF2',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: "#E7EAF2",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
